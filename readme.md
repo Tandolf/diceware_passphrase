a29de643ef9c82e85346aa62426f9dfa539814ff
Thomas Andolf <thomas.andolf@gmail.com>
2020-10-31 21:04:15 +0100

    added a basic passphrase generator

---
+++ readme.md
@@ -0,0 +1,14 @@
+# Passphrase generator using Diceware methodology
+
+This is a rust library generating passphrases using the diceware wordlists.
+
+[Diceware passphrases](https://theworld.com/~reinhold/diceware.html)
+
+TODO:
+- Create generator struct
+- select wordlists using enums
+- generate special chars
+- enable/disable capitalization of first letter in each word
+- enable/disable special chars
+- set phrase length
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,66 @@
+use std::include_str;
+use std::collections::HashMap;
+use rand::{
+    thread_rng,
+    Rng,
+};
+
+
+pub fn read_file() {
+    let file = include_str!("../resources/beale_list.txt");
+    let x: Vec<&str> = file.split("\n").collect();
+    let mut words = HashMap::new();
+    for line in x.iter() {
+        let key_value: Vec<&str> = line.split_whitespace().collect();
+        words.insert(key_value[0], key_value[1]);
+    }
+
+    
+    
+    let mut pass_phrase = String::new();
+    for _ in 1..6 {
+        let throw_str = get_n_throws(6);
+        let word = words.get(throw_str.as_str()).unwrap();
+        pass_phrase += to_uppercase(word).as_str();
+    }
+
+    println!("{}", pass_phrase);
+}
+
+pub fn get_n_throws(throws: u32) -> String {
+    // TODO: extract to own class init in Default
+    let mut rand = thread_rng();
+    let mut throw_str = String::new();
+    for _ in 1..throws {
+        let dice = rand.gen_range(1, 7);
+        throw_str += dice.to_string().as_str();
+    }
+    throw_str
+}
+
+pub fn to_uppercase(value: &str) -> String {
+    let mut chars: Vec<char> = value.chars().collect();
+    chars[0] = chars[0].to_uppercase().nth(0).unwrap();
+    let uc_word: String = chars.into_iter().collect();
+    uc_word
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::read_file;
+    use crate::to_uppercase;
+
     #[test]
     fn it_works() {
+
+        read_file();
+
         assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn should_return_uppercase_word() {
+        let word = "hello";
+        assert_eq!("Hello", to_uppercase(word));
     }
 }
